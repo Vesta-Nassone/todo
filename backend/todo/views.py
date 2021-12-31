f8908e67b6f19032022e7657562ef1bdfd2e5dea
Vesta-Nassone <vestanassone@gmail.com>
2021-12-31 22:47:42 +0200

    Reformat files using autopep8.

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
-from .serializers import TodoSerializer
+
 from .models import Todo
+from .serializers import TodoSerializer
+
 
 # Create your views here.
 class TodoView(viewsets.ModelViewSet):
