460bafc5c00596c9b0b570667967500faf7bbd33
Vesta-Nassone <vestanassone@gmail.com>
2021-12-31 22:45:24 +0200

    Remove the redundant router import.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.db import router
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
 from todo import views
-
 
 router = routers.DefaultRouter()
 router.register(r'todos', views.TodoView, 'todo')
