fa501c8bc8030daf864f66dca8d40983ef0f5f16
Vesta-Nassone <vestanassone@gmail.com>
2022-01-02 20:23:03 +0200

    Implement the handleDelete method.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,12 +40,13 @@ class App extends Component {
 				.then((res) => this.refreshList());
 			return;
 		}
-		axios.post("/api/todos/", item)
-			.then((res) => this.refreshList());
+		axios.post("/api/todos/", item).then((res) => this.refreshList());
 	};
 
 	handleDelete = (item) => {
-		alert("delete" + JSON.stringify(item));
+		axios
+			.delete(`/api/todos/${item.id}/`)
+			.then((res) => this.refreshList());
 	};
 
 	createItem = () => {
